6a6c5a2c63835588a1db51861f2e10a895bbce14
Dioni00 <dsanbla@gmail.com>
2021-01-28 19:47:18 +0100

    Queda por revisar la introducción de parámetros

--- creditcalc.py
+++ creditcalc.py
@@ -14,11 +14,82 @@ def check_payment(payment, typee):
     if typee == "diff" and payment:
         print("Incorrect parameters")
 
+# Begin Formulas
+# Monthly payment formula
 def calculating_payments(principal, periods, interest, month):
     return (principal / periods) + (interest / (12 * 100)) * (principal - ((principal * (month - 1)) / periods))
 
+# Annuity formula
+def annuity_calculating_payments(principal, periods, interest):
+    nominal_interest = interest / (100 * 12)
+    return principal * (nominal_interest * math.pow(1 + nominal_interest, periods) / (math.pow(1 + nominal_interest, periods) - 1))
+
+# Annuity formula monthly
+def annuity_calculating_payments_monthly(payment, periods, interest):
+    nominal_interest = interest / (100 * 12)
+    return payment / (nominal_interest * math.pow(1 + nominal_interest, periods) / (math.pow(1 + nominal_interest, periods) - 1))
+
+def periods_calculating(principal, payment, interest):
+    nominal_interest = interest / (100 * 12)
+    return math.log(payment / (payment - nominal_interest * principal), 1 + nominal_interest)    
+
+# End Formulas
+
+
 def last_payment(principal, periods):
     return (principal - (periods - 1) * (math.ceil(principal / periods)))
+
+def calc_monthly_payments(principal, periods, interest):
+
+    month = 1
+    payment_total = 0
+
+    while month != (periods + 1):  
+        payment_monthly = calculating_payments(principal, periods, interest, month)
+        print(f"Month {month}: payment is {math.ceil(payment_monthly)}")
+        payment_total += math.ceil(payment_monthly)
+        month += 1
+
+    pt = payment_total - principal
+    print(f"\nOverpayment = {int(pt)}")
+
+# Annuity with principal
+def calc_annuity_payments(principal, periods, interest):
+    annuity_payment = 0
+
+    annuity_payment = annuity_calculating_payments(principal, periods, interest)
+    print(f"Your annuity payment = {math.ceil(annuity_payment)}!")
+    
+    pta = (math.ceil(annuity_payment) * periods) - principal
+    print(f"\nOverpayment = {int(pta)}")    
+
+# Annuity but monthly payment
+def calc_annuity_payments_monthly(payment, periods, interest):
+
+    month = 1
+    payment_total = 0
+    loan_principal = 0
+
+    loan_principal = annuity_calculating_payments_monthly(payment, periods, interest)
+    print(f"Your annuity payment = {math.ceil(loan_principal)}!")
+    
+    pta = (payment * periods) - loan_principal
+    print(f"\nOverpayment = {int(pta)}")    
+
+
+# Calc periods to pay
+def calc_periods(principal, payment, interest):
+    periods_pay = periods_calculating(principal, payment, interest)
+    years_pay = math.ceil(periods_pay / 12)
+    print(f"It will take {years_pay} years to repay this loan!")
+
+    annuity_payment = 0
+
+    annuity_payment = annuity_calculating_payments(principal, periods_pay, interest)
+    
+    pta = (math.ceil(annuity_payment) * math.ceil(periods_pay)) - principal
+    print(f"\nOverpayment = {int(math.ceil(pta))}")        
+
 
 # Description about the script functionality
 parser = argparse.ArgumentParser(description="Payments calculator")
@@ -40,17 +111,10 @@ interest = args.interest
 
 check_type(typee)
 check_payment(payment, typee)
+#calc_monthly_payments(principal, periods, interest)
 
+#calc_annuity_payments(principal, periods, interest)
 
-# Crear una función pa esto
-month = 1
-payment_total = 0
+# calc_annuity_payments_monthly(payment, periods, interest)
 
-while month != (periods + 1):  
-    payment_monthly = calculating_payments(principal, periods, interest, month)
-    print(f"Month {month}: payment is {math.ceil(payment_monthly)}")
-    payment_total += math.ceil(payment_monthly)
-    month += 1
-
-pt = payment_total - principal
-print(f"Overpayment = {int(pt)}")
+calc_periods(principal, payment, interest)
